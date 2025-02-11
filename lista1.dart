76d2a137daad279fbaf0e060a108d701b0c108c4
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2025-02-11 16:53:52 +0000

    Hola lista

---
+++ lista1.dart
@@ -0,0 +1,15 @@
+void main() {  
+  //lista tipo entero
+  List<int> numeros=[10,20,30];
+  print(" Lista de numeros enteros:");
+print(numeros);
+print("El primer: ${numeros[0]}");  //imprime los valores numericos de acuerdo con la posición de la lista
+print("El segundo: ${numeros[1]}");
+print("El tercer: ${numeros[2]}");
+print("Elementos de la lista usando ciclo for: "); //imprime la cantidad de elementos que tiene la lista
+for(int i=0; i<numeros.length; i++){
+    print(numeros[i]);
+  }
+
+
+}  
